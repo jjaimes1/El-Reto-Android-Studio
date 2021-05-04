c1d23e948c62e0c2f73fe606d5f4bb5085b1b2de
jj <jjaimes20@gmail.com>
2021-05-04 17:43:33 +0200

    creada la ventana principal del juego, no olvidar añadir un gif de fondo de pantalla para que no se vea vacio

---
+++ app/src/main/java/com/jj/elretov1/VentanaPrincipal.java
@@ -0,0 +1,14 @@
+package com.jj.elretov1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VentanaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_principal);
+    }
+}
