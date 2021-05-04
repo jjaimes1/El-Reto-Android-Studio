4302d4e4b60e49a081b7a3257b28546db6246654
jj <jjaimes20@gmail.com>
2021-05-04 17:26:25 +0200

    creada la ventana de creacion de usuarios, ver si luego se puede añadir un boton para vincular directamente una cuenta de gmail o algo asi

---
+++ app/src/main/java/com/jj/elretov1/CreacionUsuario.java
@@ -0,0 +1,14 @@
+package com.jj.elretov1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreacionUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creacion_usuario);
+    }
+}
